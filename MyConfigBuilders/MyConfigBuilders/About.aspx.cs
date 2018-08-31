5aa43b2560b2fa02b92933229fdb89cea52524c0
Rick Anderson <riande@microsoft.com>
2018-08-31 11:22:43 -0700

    init

---
+++ MyConfigBuilders/MyConfigBuilders/About.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyConfigBuilders
+{
+    public partial class About : Page
+    {
+        public string Msg { get; set; }
+        public string ServiceKey { get; set; }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Msg = ConfigurationManager.AppSettings["ServiceID"];
+            ServiceKey = ConfigurationManager.AppSettings["ServiceKey"];
+        }
+    }
+}
